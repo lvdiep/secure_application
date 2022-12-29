bf15e677c96ff072d603cca41ab4840672a4286e
AliDiep <darkangle0307@gmail.com>
2022-12-29 10:21:06 +0700

    [PHOEN-4132] Added: protect sensitive data when app in background

--- lib/secure_application_native.dart
+++ lib/secure_application_native.dart
@@ -4,16 +4,13 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 class SecureApplicationNative {
-  static const MethodChannel _channel =
-      const MethodChannel('secure_application');
+  static const MethodChannel _channel = const MethodChannel('secure_application');
 
   static void registerForEvents(VoidCallback lock, VoidCallback unlock) {
-    _channel.setMethodCallHandler(
-        (call) => secureApplicationHandler(call, lock, unlock));
+    _channel.setMethodCallHandler((call) => secureApplicationHandler(call, lock, unlock));
   }
 
-  static Future<dynamic> secureApplicationHandler(
-      MethodCall methodCall, lock, unlock) async {
+  static Future<dynamic> secureApplicationHandler(MethodCall methodCall, lock, unlock) async {
     switch (methodCall.method) {
       case 'lock':
         lock();
